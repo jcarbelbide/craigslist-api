5d1ca06b9b11be08c3e9c9d9fe9d23150c3366eb
jcarbelbide <jcarbelbide@gmail.com>
2022-03-26 18:31:39 -0700

    More changes to cors

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'application/json'
 
 
-@cross_origin()
 @app.route("/api/<location>/<sub_location>/<board>/<starting_index>/<query>", methods=['GET', 'OPTIONS'])
+@cross_origin(origin='*',headers=['Content-Type', 'Authorization'])
 def craigslist_api(location, sub_location, board, starting_index, query):
+
     json_list = get_craigslist_listings(location, sub_location, board, starting_index, query)
 
     resp = Response(response=json_list,
                     status=200,
                     mimetype="application/json")
+    resp.headers.add('Access-Control-Allow-Origin', '*')
 
     return resp
 
